7f885ef6fd3067519cf4179f76b44f082acbee2b
Sofie Bluhm Kristensen <eaa23sokr@students.eaaa.dk>
2024-05-21 11:07:43 +0200

    filer

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -41,3 +41,48 @@ function tilbage(){
     galleribillede.pop();
     galleribillede[0].style.display = "block";
 }
+
+
+
+// antal gavekort
+
+const minusButtons = document.getElementById("minus");
+const plusButtons = document.getElementById("plus");
+const antal = document.getElementById("antal")[0];
+
+// Da der kun er et antal-element brug [0] for at få det første element
+
+let tal = 0;
+
+// gyudon knap
+
+// Opdaterer det aktuelle antal, der vises:
+function opdaterAntal() {
+    antal.innerHTML = tal;
+  }
+  
+  // Event listener - minus-knap:
+  for (let i = 0; i < minusButtons.length; i++) {
+    minusButtons[i].addEventListener("click", traekfra);
+  }
+  
+  // Event handler - trækker fra, men ikke mindre end 0:
+  function traekfra() {
+    if (tal > 0) {
+      tal--;
+      opdaterAntal();
+    }
+  }
+  // Event listener - plus-knapperne:
+  
+  for (let i = 0; i < plusButtons.length; i++) {
+    plusButtons[i].addEventListener("click", laegtil);
+  }
+  
+  // Event handler - lægger til, men ikke mere end 6:
+  function laegtil() {
+    if (tal < 6) {
+      tal++;
+      opdaterAntal();
+    }
+  }
