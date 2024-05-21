82285a5a5d2099a2edd1adde91acfcd990482d99
Sofie Bluhm Kristensen <eaa23sokr@students.eaaa.dk>
2024-05-21 23:37:52 +0200

    js

--- javascript/infographic.js
+++ javascript/infographic.js
@@ -1,3 +1,65 @@
+const info_per = document.querySelectorAll("#info_per");
+
+window.onscroll = fadeIndUd;
+
+let info_list = 0;
+
+function fadeIndUd() {
+    let info_pla1 = info_per[0].getBoundingClientRect();
+    if(info_pla1.top >= window.innerHeight - 200) {
+        info_per[0].style.opacity = "0";
+    }
+    if(info_pla1.top < window.innerHeight - 150) {
+        info_per[0].style.opacity = "1";
+    }
+    let info_pla2 = info_per[1].getBoundingClientRect();
+    if(info_pla2.top >= window.innerHeight - 200) {
+        info_per[1].style.opacity = "0";
+    }
+    if(info_pla2.top < window.innerHeight - 150) {
+        info_per[1].style.opacity = "1";
+    }
+    let info_pla3 = info_per[2].getBoundingClientRect();
+    if(info_pla3.top >= window.innerHeight - 150) {
+        info_per[2].style.opacity = "0";
+    }
+    if(info_pla3.top < window.innerHeight - 150) {
+        info_per[2].style.opacity = "1";
+    }
+    let info_pla4 = info_per[3].getBoundingClientRect();
+    if(info_pla4.top >= window.innerHeight - 150) {
+        info_per[3].style.opacity = "0";
+    }
+    if(info_pla4.top < window.innerHeight - 150) {
+        info_per[3].style.opacity = "1";
+    }
+    let info_pla5 = info_per[4].getBoundingClientRect();
+    if(info_pla5.top >= window.innerHeight - 150) {
+        info_per[4].style.opacity = "0";
+    }
+    if(info_pla5.top < window.innerHeight - 150) {
+        info_per[4].style.opacity = "1";
+    }
+    let info_pla6 = info_per[5].getBoundingClientRect();
+    if(info_pla6.top >= window.innerHeight - 150) {
+        info_per[5].style.opacity = "0";
+    }
+    if(info_pla6.top < window.innerHeight - 150) {
+        info_per[5].style.opacity = "1";
+    }
+    let info_pla7 = info_per[6].getBoundingClientRect();
+    if(info_pla7.top >= window.innerHeight - 150) {
+        info_per[6].style.opacity = "0";
+    }
+    if(info_pla7.top < window.innerHeight - 150) {
+        info_per[6].style.opacity = "1";
+    }
+    
+}
+
+
+
+
 // informationsboks ved exoplaneter:
 const info = document.getElementById("pin1");
 const overlay = document.getElementById("overlay_container_1");
